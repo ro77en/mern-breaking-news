a9901a1e34ba8db23037515e41b49457890b3f85
ro77en <lucas.mdantas@icloud.com>
2024-12-17 22:20:33 -0300

    refactor: add soma route

--- api/index.js
+++ api/index.js
@@ -6,6 +6,11 @@ app.get('/', function (req, res) {
   res.send('Hello World')
 })
 
+app.get('/soma', (req, res) => {
+    const soma = 100 + 1;
+    res.send({soma:soma});
+})
+
 app.listen(port, () => {
     console.log(`server running on http://localhost:${port}`);
 })
