69df270813349200bcc27c8a865861b18e2e50a6
ro77en <dantas.lucas@rfb.gov.br>
2024-12-20 10:29:00 -0300

    refactor: route config, create user and user controller

--- api/index.js
+++ api/index.js
@@ -4,7 +4,8 @@ const userRoute = require("./src/routes/user.route");
 const app = express();
 const port = 8080;
 
-app.use("/soma", userRoute);
+app.use(express.json());
+app.use("/user", userRoute);
 
 app.listen(port, () => {
   console.log(`server running on http://localhost:${port}`);
--- api/src/controllers/user.controller.js
+++ api/src/controllers/user.controller.js
@@ -1,6 +1,20 @@
-const soma = (req, res) => {
-  const soma = 100 + 1;
-  res.send({ soma: soma });
+const create = (req, res) => {
+  const { name, userName, email, password, avatar, background } = req.body;
+
+  if (!name || !userName || !email || !password || !avatar || !background) {
+    res.status(400).send({ message: "Submit all fields for signin up" });
+  }
+
+  res.status(201).send({
+    message: "User created successfuly!",
+    user: {
+      name,
+      userName,
+      email,
+      avatar,
+      background,
+    },
+  });
 };
 
-module.exports = { soma };
+module.exports = { create };
