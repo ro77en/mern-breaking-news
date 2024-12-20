cb3af32f0ed70128bfeb6430f6b9d7bddb8b9947
ro77en <lucas.mdantas@icloud.com>
2024-12-20 11:52:33 -0300

    refactor: change user controller "create" function name to "createUser"

--- api/src/routes/user.route.js
+++ api/src/routes/user.route.js
@@ -1,6 +1,6 @@
 const route = require("express").Router();
 const userController = require("../controllers/user.controller");
 
-route.post("/", userController.create);
+route.post("/", userController.createUser);
 
 module.exports = route;
