fb5698601528bb337897b3e2d29ad1cbe083f75b
ro77en <lucas.mdantas@icloud.com>
2024-12-23 11:10:51 -0300

    feat: add bcryptjs to UserSchema for password hashing

--- api/src/models/User.js
+++ api/src/models/User.js
@@ -1,4 +1,5 @@
 import mongoose from "mongoose";
+import bcrypt from 'bcryptjs';
 
 const UserSchema = new mongoose.Schema({
   name: {
@@ -29,6 +30,11 @@ const UserSchema = new mongoose.Schema({
     type: String,
     required: true,
   },
+});
+
+UserSchema.pre('save', async function(next) {
+  this.password = await bcrypt.hash(this.password, 10);
+  next();
 });
 
 const User = mongoose.model("User", UserSchema);
