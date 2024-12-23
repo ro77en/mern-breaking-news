a8682932bf5ad55bb59c849ca609bd33c02063dd
ro77en <lucas.mdantas@icloud.com>
2024-12-23 09:37:53 -0300

    feat: add middleware to validate user

--- api/src/routes/user.route.js
+++ api/src/routes/user.route.js
@@ -1,12 +1,14 @@
 const route = require("express").Router();
 const userController = require("../controllers/user.controller");
 
+const {validId, validUser} = require('../middlewares/global.middlewares');
+
 route.get('/', userController.getAllUsers);
 
-route.get('/:id', userController.getUserById);
+route.get('/:id', validId, validUser, userController.getUserById);
 
 route.post("/", userController.createUser);
 
-route.patch('/:id', userController.updateUser);
+route.patch('/:id', validId, validUser, userController.updateUser);
 
 module.exports = route;
