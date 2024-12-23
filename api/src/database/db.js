8ec18750d277e447da3afa0499f59b3d201c81ed
ro77en <lucas.mdantas@icloud.com>
2024-12-23 11:27:11 -0300

    refactor: update db connection to use full URI from environment variable

--- api/src/database/db.js
+++ api/src/database/db.js
@@ -1,12 +1,9 @@
-import dotenv from 'dotenv';
 import mongoose from 'mongoose';
-
-dotenv.config();
 
 const connectDatabase = () => {
   console.log("connecting database...");
 
-  const uri = `mongodb+srv://ro77en:${process.env.DB_PASSWORD}@cluster0.vqtrg.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+  const uri = process.env.DB_URI;
 
   mongoose
     .connect(uri)
