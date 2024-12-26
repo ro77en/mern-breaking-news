e56e5c895da73ec53147f6b96c03e889bac16609
ro77en <lucas.mdantas@icloud.com>
2024-12-26 17:18:22 -0300

    feat: use auth middleware in news route

--- api/src/routes/news.route.js
+++ api/src/routes/news.route.js
@@ -1,10 +1,11 @@
 import { Router } from "express";
 import newsController from "../controllers/news.controller.js";
+import { authMiddleware } from "../middlewares/auth.middleware.js";
 
 const router = Router();
 
-router.get("/", newsController.getAllPosts);
+router.get("/", authMiddleware, newsController.getAllPosts);
 
-router.post("/", newsController.createPost);
+router.post("/", authMiddleware, newsController.createPost);
 
 export default router;
