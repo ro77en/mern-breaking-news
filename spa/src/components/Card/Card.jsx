e44930c8282aafabb3f59ca6580c2ba24b9401f9
ro77en <lucas.mdantas@icloud.com>
2025-01-14 10:27:11 -0300

    feat: add Card styled components

--- spa/src/components/Card/Card.jsx
+++ spa/src/components/Card/Card.jsx
@@ -1,16 +1,26 @@
-import { Section } from "./CardStyled";
+import { CardContainer, CardContent, CardInteractions} from "./CardStyled";
 
 export function Card({ post }) {
-  console.log(post);
   return (
-    <Section>
-      <h2>{post.title}</h2>
-      <p>{post.text}</p>
-      <img src={post.image} alt="img" />
-      <i className="bi bi-hand-thumbs-up"></i>
-      <span>{post.likes}</span>
-      <i className="bi bi-chat-dots"></i>
-      <span>{post.comments}</span>
-    </Section>
+    <CardContainer>
+      <CardContent>
+        <div>
+          <h2>{post.title}</h2>
+          <p>{post.text}</p>
+        </div>
+        <img src={post.image} alt="img" />
+      </CardContent>
+
+      <CardInteractions>
+        <div>
+          <i className="bi bi-hand-thumbs-up"></i>
+          <span>{post.likes}</span>
+        </div>
+        <div>
+          <i className="bi bi-chat-dots"></i>
+          <span>{post.comments}</span>
+        </div>
+      </CardInteractions>
+    </CardContainer>
   );
 }
--- spa/src/components/Card/CardStyled.jsx
+++ spa/src/components/Card/CardStyled.jsx
@@ -1,5 +1,40 @@
 import styled from "styled-components";
 
-export const Section = styled.section`
-  margin-top: 9rem;
+export const CardContainer = styled.section`
+  display: flex;
+  flex-direction: column;
+  gap: 1rem;
+  max-width: 100%;
+
+  box-shadow: #32326926 0px 2px 5px 0px, rgba(0, 0, 0, 0.05) 0px 1px 1px 0px;
+  border-radius: 0.3rem;
+  background-color: #fff;
+  padding: 2rem;
 `;
+
+export const CardContent = styled.article`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  gap: 1rem;
+
+  & h2 {
+    margin-bottom: 1rem;
+  }
+
+  & img {
+    width: 30%;
+    object-fit: cover;
+    object-position: center;
+  }
+`;
+
+export const CardInteractions = styled.article`
+  display: flex;
+  align-items: center;
+  gap: 1rem;
+
+  & i {
+    margin-right: 0.3rem;
+  }
+`
