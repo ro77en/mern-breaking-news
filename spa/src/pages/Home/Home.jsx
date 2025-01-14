0d59ec81dc918210d28fce0af26ea8a5a6737646
ro77en <lucas.mdantas@icloud.com>
2025-01-14 10:26:46 -0300

    feat: add HomeBody styled component

--- spa/src/pages/Home/Home.jsx
+++ spa/src/pages/Home/Home.jsx
@@ -1,14 +1,17 @@
 import { Card } from "../../components/Card/Card";
 import { Navbar } from "../../components/Navbar/Navbar";
 import { news } from "../../Data";
+import { HomeBody } from "./HomeStyled";
 
 export default function Home() {
   return (
     <>
       <Navbar />
-      {news.map((post, index) => {
-        return <Card key={index} post={post} />;
-      })}
+      <HomeBody>
+        {news.map((post, index) => {
+          return <Card key={index} post={post} />;
+        })}
+      </HomeBody>
     </>
   );
 }
---
+++ spa/src/pages/Home/HomeStyled.jsx
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+
+export const HomeBody = styled.section`
+    display: grid;
+    grid-template-columns: repeat(2, 1fr); 
+    grid-gap: 1rem;
+
+    width: 80%;
+    margin: 0 auto;
+    margin-top: 9rem;
+`
