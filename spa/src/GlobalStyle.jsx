b5100d6faabf4c2c4108425d49d5626cef2f815a
ro77en <lucas.mdantas@icloud.com>
2025-01-13 21:51:43 -0300

    refact: add GlobalStyle component

--- spa/src/GlobalStyle.jsx
+++ spa/src/GlobalStyle.jsx
@@ -1,3 +1,6 @@
+import { createGlobalStyle } from "styled-components";
+
+export const GlobalStyle = createGlobalStyle`
 * {
   margin: 0;
   padding: 0;
@@ -14,3 +17,4 @@ body {
   height: 100vh;
   background-color: #f5f5f5;
 }
+`;
